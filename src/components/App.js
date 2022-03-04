9961d49e1b675ab8e52d310af32bb4cef3b4a0e5
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-03-04 19:14:14 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "../styles/App.css";
 
-import '../styles/App.css';
-
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
-const App = () => {
-
-  
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta"
+];
+function App() {
+  const [searchValue, setSearchValue] = useState(" ");
   return (
     <div id="main">
-      
-    </div>
-  )
-}
+      <input
+        type="text"
+        name="search"
+        id="search-input"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
 
+      <ul>
+        {searchArray.map((name) => {
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
